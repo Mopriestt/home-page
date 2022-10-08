31d77979ecb9a8b1c7076d49c468db93ddf4670c
yijunl <mopriestt@gmail.com>
2022-10-08 11:50:07 +1100

    Format

--- lib/grid_card.dart
+++ lib/grid_card.dart
@@ -4,10 +4,22 @@ class GridCard extends StatelessWidget {
   final Widget child;
   final VoidCallback onClick;
   final Color? color;
-  const GridCard({super.key, required this.child, required this.onClick, this.color,});
+
+  const GridCard({
+    super.key,
+    required this.child,
+    required this.onClick,
+    this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: onClick, child: Card(color: color,child: child,));
+    return GestureDetector(
+      onTap: onClick,
+      child: Card(
+        color: color,
+        child: child,
+      ),
+    );
   }
 }
